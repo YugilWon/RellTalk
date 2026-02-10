baebe62df5e87ab121efed66ca3ceb26db3624a8
YuGil <yugil3150@naver.com>
2026-02-10 13:52:27 +0900

    Style: 모바일 ui 수정

--- components/page/MovieList.tsx
+++ components/page/MovieList.tsx
@@ -4,7 +4,15 @@ import { Movie } from "@/(types)/interface";
 
 export default function MovieList({ movies }: { movies: Movie[] }) {
   return (
-    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <ul
+      className="
+     grid grid-cols-3
+     sm:grid-cols-3
+     md:grid-cols-4
+     lg:grid-cols-5
+     gap-3 sm:gap-4
+    "
+    >
       {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
